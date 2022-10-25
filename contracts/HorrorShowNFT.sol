07983237a964ce828de9ee3435fe8fa777855c58
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-25 18:57:03 -0400

    whitelist functions//minting functions//modifiers

--- contracts/HorrorShowNFT.sol
+++ contracts/HorrorShowNFT.sol
@@ -14,46 +14,97 @@ import "hardhat/console.sol";
 contract HorrorShow is ERC721, ERC721URIStorage{
     using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    Counters.Counter public _tokenIdCounter;
 
     address public admin;
+
+    // contract pause boolean
+    bool public contractPaused = false;
 
     // base uri
     string public baseIPFS;
 
     // whitelist 1
-    address[] public whitelist1;
-    uint256 public whitelist1MaxMint;
+    address[] public whitelist;
+    uint256 public whitelistMaxMint;
+    mapping(address=>bool) public canWhitelistMint;
 
-    // white list 2
-    address[] public whitelist2;
-    uint256 public whitelist2MaxMint;
-
-
+    // modifiers
     modifier onlyAdmin {
         require(msg.sender == admin, "Not admin of contract");
         _;
     }
 
+    modifier isPaused(){
+        require(contractPaused == false, "Contract is currently paused");
+        _;
+    }
+
+    modifier onlyWhitelist(){
+        require(canWhitelistMint[msg.sender] == true, "Not on whitelist" );
+        _;
+    }
+
+
     constructor()ERC721("HorrorShow", "HRS"){
         admin = payable(msg.sender);
     }
 
-    function addToWhitelist1(address newWhitelist) public onlyAdmin{
-        whitelist1.push(newWhitelist);
-    }
-    function addToWhitelist2(address newWhitelist) public onlyAdmin{
-        whitelist2.push(newWhitelist);
+    // white list functions
+
+    function addToWhitelist(address newWhitelist) public onlyAdmin{
+        whitelist.push(newWhitelist);
+        canWhitelistMint[newWhitelist] = true;
     }
 
-    // minting function
+    function removeFromWhitelist(address removedAddress) public onlyAdmin{
+        canWhitelistMint[removedAddress] = false;
+    }
 
-    function safeMint(address to, string memory uri) public {
-        uint256 tokenId = _tokenIdCounter.current();
+    // set whitelist max mint amount
+    function setWhitelistMintLimit(uint newLimit) public onlyAdmin{
+        whitelistMaxMint = newLimit;
+    }
+
+    // pause contract function 
+    function pauseContract() public onlyAdmin {
+        require(contractPaused == false, "contract is currently live");
+        contractPaused = true;
+    }
+
+    function unpauseContract() public onlyAdmin{
+        require(contractPaused == true, "contract is currently paused");
+        contractPaused = false;
+    }
+
+
+
+
+    // whitelist minting function
+
+   function whitelistMint(address to, string memory uri) public onlyWhitelist {
+        require(_tokenIdCounter.current() < whitelistMaxMint, "max whitelist minted already");
         _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
+
+
+
+
+    // minting function
+
+    function safeMint(address to, string memory uri) public isPaused {
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+
+
+    
 
     function _baseURI() internal view override returns (string memory) {
         return baseIPFS;
@@ -61,10 +112,14 @@ contract HorrorShow is ERC721, ERC721URIStorage{
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) isPaused {
         super._burn(tokenId);
     }
 
+
+
+
+    // get tokenURI
     function tokenURI(uint256 tokenId)
         public
         view
--- test/horrorshowTest.js
+++ test/horrorshowTest.js
@@ -4,6 +4,8 @@ const { ethers } = require("hardhat");
 describe("Horror Show NFT Contract", () =>{
     let HorrorShowContract, deployer, user1, user2
 
+    const SAMPLE_URI = "SAMPLE URI"
+
     beforeEach(async ()=>{
         const horrorContractFactory = await ethers.getContractFactory("HorrorShow")
         HorrorShowContract = await horrorContractFactory.deploy()
@@ -20,7 +22,7 @@ describe("Horror Show NFT Contract", () =>{
         console.log(`Horror Show deployed to ${HorrorShowContract.address}`)
     })
     it("checks the owner of the contract", async ()=>{
-        expect(await HorrorShowContract.owner()).to.equal(deployer.address)
+        expect(await HorrorShowContract.admin()).to.equal(deployer.address)
     })
     it("checks the collection name", async () =>{
         expect(await HorrorShowContract.name()).to.equal("HorrorShow")
@@ -28,5 +30,76 @@ describe("Horror Show NFT Contract", () =>{
     it("checks the symbol", async () =>{
         expect(await HorrorShowContract.symbol()).to.equal("HRS")
     })
-    it("checks the ")
+    it("checks the current count", async () =>{
+        expect(await HorrorShowContract._tokenIdCounter()).to.equal(0)
+    })
+    describe("whitelist and mint functions", () =>{
+        beforeEach(async () =>{
+            await HorrorShowContract.connect(deployer).addToWhitelist(user1.address);
+            await HorrorShowContract.connect(deployer).setWhitelistMintLimit(10)
+        })
+        it("checks the user is added to whitelist", async ()=>{
+            expect(await HorrorShowContract.canWhitelistMint(user1.address)).to.equal(true)
+        })
+        it("checks the add to whitelist fail case", async () =>{
+            await expect( HorrorShowContract.connect(user1).addToWhitelist(user2.address)).to.be.reverted
+        })
+        it("checks the whitelist max mint limit", async () =>{
+            expect(await HorrorShowContract.whitelistMaxMint()).to.equal(10)
+        })
+        it("checks the address whitelist was removed", async () =>{
+            await HorrorShowContract.connect(deployer).removeFromWhitelist(user1.address)
+            expect(await HorrorShowContract.canWhitelistMint(user1.address)).to.equal(false)
+        })
+        it("checks the contract was paused", async () =>{
+            await HorrorShowContract.connect(deployer).pauseContract()
+            expect(await HorrorShowContract.contractPaused()).to.equal(true)
+        })
+        it("checks the contract paused fail case", async()=>{
+            console.log("test for paused contract fail case")
+        })
+    })
+    describe("whitelist minting functions", () =>{
+        beforeEach(async () =>{
+            await HorrorShowContract.connect(deployer).addToWhitelist(user1.address);
+            await HorrorShowContract.connect(deployer).setWhitelistMintLimit(3)
+            await HorrorShowContract.connect(user1).whitelistMint(user1.address, SAMPLE_URI)
+        })
+        it("checks the token count", async () =>{
+            expect(await HorrorShowContract._tokenIdCounter()).to.equal(1)
+        })
+        it("checks the token uri", async () =>{
+            expect(await HorrorShowContract.tokenURI(1)).to.equal(SAMPLE_URI)
+        })
+        it("checks the owner of the token", async () =>{
+            expect(await HorrorShowContract.balanceOf(user1.address)).to.equal(1)
+        })
+        it("checks the whitelist mint fail case", async () =>{
+            await expect(HorrorShowContract.connect(user2).whitelistMint(user2.address, SAMPLE_URI)).to.be.reverted
+        })
+        it("checks the max whitelist mint fail case", async () =>{
+            await HorrorShowContract.connect(user1).whitelistMint(user1.address, SAMPLE_URI)
+            await HorrorShowContract.connect(user1).whitelistMint(user1.address, SAMPLE_URI)
+            await expect( HorrorShowContract.connect(user1).whitelistMint(user1.address, SAMPLE_URI)).to.be.reverted
+        })
+        
+    })
+    describe("full mint functions", async () =>{
+        beforeEach(async () =>{
+            await HorrorShowContract.connect(user2).safeMint(user2.address, SAMPLE_URI)
+        })
+        it("checks the token count", async () =>{
+            expect(await HorrorShowContract._tokenIdCounter()).to.equal(1)
+        })
+        it("checks the token uri", async () =>{
+            expect(await HorrorShowContract.tokenURI(1)).to.equal(SAMPLE_URI)
+        })
+        it("checks the owner of the token", async () =>{
+            expect(await HorrorShowContract.balanceOf(user2.address)).to.equal(1)
+        })
+        it("checks the paused contract fail case", async () =>{
+            await HorrorShowContract.connect(deployer).pauseContract()
+            await expect(HorrorShowContract.safeMint(user2.address, SAMPLE_URI)).to.be.reverted
+        })
+    })
 })
